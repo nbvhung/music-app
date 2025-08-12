76e26f1d6ef33a8db52dcae248d751b4f5ada485
nbvhung <nguyenbaviethung300805@gmail.com>
2025-08-12 10:31:36 +0700

    Cài pug

--- index.ts
+++ index.ts
@@ -3,10 +3,13 @@ import express, { Express, Request, Response } from "express"
 const app: Express = express();
 const port: number = 3000;
 
-app.get("/topic", (req: Request, res: Response) => {
-    res.send("Chủ đề bài hát");
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/topics", (req: Request, res: Response) => {
+    res.render("client/pages/topics/index");
 });
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
-})
+});
