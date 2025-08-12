0f84596b2a8b73f93ea8613df8504208e1e93744
nbvhung <nguyenbaviethung300805@gmail.com>
2025-08-12 10:20:33 +0700

    Tạo router cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello world")
+import express, { Express, Request, Response } from "express"
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/topic", (req: Request, res: Response) => {
+    res.send("Chủ đề bài hát");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
