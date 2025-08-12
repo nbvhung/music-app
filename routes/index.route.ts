7212856adce47b725ef00ac76f5217c165e3306e
nbvhung <nguyenbaviethung300805@gmail.com>
2025-08-12 11:45:44 +0700

    Tạo routes

---
+++ routes/index.route.ts
@@ -0,0 +1,10 @@
+import { Express } from "express";
+import { topicRoutes } from "./topic.route";
+
+const clientRoutes = (app: Express) => {
+
+    app.use("/topics", topicRoutes);
+
+};
+
+export default clientRoutes;
